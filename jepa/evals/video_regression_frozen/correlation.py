ae4e7cc53b2a640374a6494fa6b3fe73dc1bfc29
seanchenjiale <jl.seanchen@gmail.com>
2026-01-20 15:46:13 +0800

    adding MAE to validation

--- jepa/evals/video_regression_frozen/correlation.py
+++ jepa/evals/video_regression_frozen/correlation.py
@@ -12,12 +12,13 @@ def correlation(csv_path):
     correlation = pd.Series(original_label).corr(pd.Series(predicted_label))
     
     output_png = csv_path.replace('.csv', '_correlation_plot.png')
+    mae = pd.Series(original_label).sub(pd.Series(predicted_label)).abs().mean()
     plt.figure(figsize=(8, 6))
     plt.scatter(original_label, predicted_label, alpha=0.5)
-    plt.title(f'Scatter Plot of Original vs Predicted Labels\nCorrelation: {correlation:.4f}')
+    plt.title(f'Scatter Plot of Original vs Predicted Labels\nCorrelation: {correlation:.4f} , MAE: {mae:.4f}') 
     plt.xlabel('Original Labels')
     plt.ylabel('Predicted Labels')
     plt.savefig(output_png)
-    print(f"Saved correlation plot to {output_png}\nCorrelation: {correlation:.4f}")
+    print(f"Saved correlation plot to {output_png}\nCorrelation: {correlation:.4f}, MAE: {mae:.4f}'")
     plt.close()
     return correlation
